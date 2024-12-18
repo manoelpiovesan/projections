4fb64e66aa6198b178e84f0af8cfbf21345d4278
manoel <manoelpiovesan@gmail.com>
2024-12-18 16:36:11 -0300

    Refactoring some structures

--- src/main/java/org/acme/repositories/CategoryRepository.java
+++ src/main/java/org/acme/repositories/CategoryRepository.java
@@ -5,29 +5,74 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.acme.entities.Category;
 import org.acme.projections.CategoryProjection;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class CategoryRepository extends AbstractRepository<Category> {
 
-    @Override
+    /*
+     *  Used to list entities projected.
+     *
+     * */
+    public List<CategoryProjection> listProjected(String term, int page,
+                                                  int size) {
+        Map<String, Object> params = new HashMap<>();
+
+        /*
+         * Here, we have to join the Category and Product tables
+         * to calculate the product count, absolute product
+         * count, and average price for project. We also have
+         * to group by the category fields and order by the
+         * category name.
+         * */
+        StringBuilder hql =
+                new StringBuilder(
+                        "FROM Category c LEFT JOIN Product p ON " +
+                        "c.id = p.category.id");
+
+        if (term != null) {
+            hql.append(" WHERE c.name like :term");
+            params.put("term", "%" + term + "%");
+        }
+
+        hql.append(" GROUP BY c.id, c.name, c.description, c.image");
+        hql.append(" ORDER BY c.name");
+
+        return find(hql.toString(), params).page(page, size)
+                                           .project(CategoryProjection.class)
+                                           .list();
+    }
+
+    /*
+     * Used to list entities.
+     * @param term The term to search for.
+     * @param page The page to list.
+     * @param size The size of the page.
+     */
+    public List<Category> list(String term, int page, int size) {
+        return search(term).page(page, size).list();
+    }
+
+    /*
+     * Used to search for entities by a term.
+     *  @param term The term to search for.
+     */
     public PanacheQuery<Category> search(String term) {
         if (term == null) {
             return findAll();
-        } else {
-            return find("""
-                        FROM Category c LEFT JOIN Product p ON c.id = p.category.id
-                        WHERE c.name
-                        LIKE ?1
-                        OR c.description LIKE ?1
-                        GROUP BY c.id, c.name, c.description, c.image
-                        """, "%" + term + "%");
-
         }
+
+        return find("name like ?1", "%" + term + "%");
     }
 
-    public List<CategoryProjection> listProjected(String term) {
-        return search(term).project(CategoryProjection.class).list();
+    /*
+     * Used to count entities.
+     * @param term The term to search for.
+     */
+    public long count(String term) {
+        return search(term).count();
     }
 
 }
--- src/main/java/org/acme/resources/CategoryResource.java
+++ src/main/java/org/acme/resources/CategoryResource.java
@@ -4,6 +4,7 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.acme.entities.Category;
 import org.acme.projections.CategoryProjection;
 import org.acme.repositories.CategoryRepository;
@@ -18,21 +19,28 @@ public class CategoryResource {
     CategoryRepository repository;
 
     @GET
-    @Path("/projected")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<CategoryProjection> listProjected(
-            @QueryParam("term") String term) {
-        return repository.listProjected(term);
-    }
+    public Response list(
+            @QueryParam("page") @DefaultValue("0") int page,
+            @QueryParam("size") @DefaultValue("15") int size,
+            @QueryParam("projected") @DefaultValue("false") boolean projected,
+            @QueryParam("term") String term
+    ) {
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Category> list(
-            @QueryParam("first") int first,
-            @QueryParam("pageSize") int pageSize,
-            @QueryParam("id") Long id,
-            @QueryParam("term") String term) {
-        return repository.list(first, pageSize, id, term);
+        if (page < 0 || size < 0) {
+            return Response.status(Response.Status.BAD_REQUEST.getStatusCode(),
+                                   "Page and size must be " +
+                                   "greater than or equal to 0.")
+                           .build();
+        }
+
+        if (projected) {
+            return Response.ok(repository.listProjected(term, page, size))
+                           .build();
+        }
+
+        return Response.ok(repository.list(term, page, size))
+                       .build();
     }
 
     @GET
